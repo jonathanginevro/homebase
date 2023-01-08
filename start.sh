70bc7ab788137cd0eaf8694b19156a82602262c6
Jonathan <jonathanginevro@outlook.com>
2023-01-07 23:02:36 -0500

    final

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 pip install pipenv
+python3 -m virtualenv venv
+source venv/bin/activate
 pip install Django
 pip install requests
 pip install lxml
 pip install beautifulsoup4
-python3 -m virtualenv venv
-source venv/bin/activate
 ./homebase/manage.py migrate
 ./homebase/manage.py runserver
